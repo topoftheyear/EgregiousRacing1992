700b2eff25e9b1155c0ceb98ef1d58e4c4c3ab03
Cinnamon Toast <topoftheyear@Gmail.com>
2021-08-28 20:31:36 -0500

    Updated build process

--- build.bat
+++ build.bat
@@ -1,11 +1,15 @@
 @ECHO off
 ::Images have to be done specially
-python -O -m PyInstaller -w --noconfirm --name voxel^
+python -O -m PyInstaller -w --noconfirm --name "Egregious Racing 1992"^
+    --icon=icon.ico^
     --path="./common"^
-    --path="./csource"^
     --path="./utils"^
-    --add-data="./img/1C.png;img"^
-    --add-data="./img/1H.png;img"^
+    --add-data="./img/*;img"^
     --add-data="./liblines.dll;."^
     --add-data="./settings.ini;."^
+    --add-data="./leaderboard.json;."^
+    --add-data="./visitor2.ttf;."^
+    --add-data="./README.md;."^
+    --clean main.py^
     main.py
+pause
