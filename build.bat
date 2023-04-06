218ac7d3b98a085dbb1b80bedb0d766a03f1f7db
Cinnamon Toast <topoftheyear@Gmail.com>
2023-04-06 01:40:54 -0500

    Forgot the build.bat because I confused it for the proper builds folder

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @ECHO off
 ::Images have to be done specially
-python -O -m PyInstaller -w --noconfirm --name "Egregious Racing 1992"^
+py -O -m PyInstaller -w --noconfirm --name "Egregious Racing 1992"^
     --icon=icon.ico^
     --path="./common"^
     --path="./utils"^
     --add-data="./img/*;img"^
-    --add-data="./liblines.dll;."^
+    --add-data="./dll/*;dll"^
     --add-data="./settings.ini;."^
     --add-data="./leaderboard.json;."^
     --add-data="./visitor2.ttf;."^
